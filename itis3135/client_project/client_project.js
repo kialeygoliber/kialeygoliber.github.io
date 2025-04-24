8bb47a397b09122475bf65223e8ddb99f4c8b4e6
kialeygoliber <kialeygoliber@gmail.com>
2025-04-23 21:38:09 -0400

    Client Project part 4

--- itis3135/client_project/client_project.js
+++ itis3135/client_project/client_project.js
@@ -5,12 +5,56 @@ function changePage(url) {
 }
 
 
-function show(sectionID){
-    document.querySelectorAll("section").forEach((section) => {
-        section.classList.remove("active");
+//CONTACT US- page submit button "submits form"
 
-    });
+function SubmitContactForm(event) {
 
-    const sectionToShow = document.getElementById(sectionID);
-    sectionToShow.classList.add("active");
+    event.preventDefault();
+    document.getElementById("contact-form").addEventListener("submit", function(e) {
+        e.preventDefault();
+    }, 
+    document.getElementById("contact-message").textContent = "Thanks for the Info! Dylan or a team member will be in touch soon!!");
+    document.getElementById("contact-form").style.display = "none";
+}
+
+//TESTIMONIALS- slideshow with arrows (moves automatically)
+
+const boxes = document.querySelectorAll(".box");
+let currentIndex = 0;
+
+function updateBoxes() {
+    for (let i = 0; i < boxes.length; i++ ) {
+        boxes[i].classList.remove("active", "next", "previous");
+
+        if (i === currentIndex) {
+            boxes[i].classList.add("active");
+        } else if (i === (currentIndex + 1) % boxes.length) {
+            boxes[i].classList.add("next");
+        } else if (i === (currentIndex - 1 + boxes.length) % boxes.length) {
+            boxes[i].classList.add("previous");
+        }
+    }
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % boxes.length;
+    updateBoxes();
+}
+
+function previousSlide() {
+    currentIndex = (currentIndex - 1 + boxes.length) % boxes.length;
+    updateBoxes();
+}
+
+setInterval(nextSlide, 5000);
+updateBoxes();
+
+//HOME PAGE- Accordian Style paragpraphs
+
+let clicked = false;
+
+function dropdown(className) {
+    const section = document.querySelector("." + className);
+    section.classList.toggle("inactive");
+    
 }
